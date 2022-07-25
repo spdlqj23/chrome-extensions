f20caa9e83ab9df4b388b81a8e589e556d45f36c
kim.jiho02 <kim.jiho02@eland-partner.co.kr>
2022-07-25 13:21:47 +0900

    02 hello world

---
+++ 02_hello_world/background.js
@@ -0,0 +1,8 @@
+
+chrome.runtime.onInstalled.addListener(async () => {
+    let url = chrome.runtime.getURL("hello.html");
+
+    let tab = await chrome.tabs.create({ url });
+
+    console.log(`Created tab ${tab.id}, url: ${url}`)
+})
